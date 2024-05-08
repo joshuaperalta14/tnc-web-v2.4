8f27b886b57695c7b09ec62742bd571b672815d3
joshuaperalta14 <148190702+joshuaperalta14@users.noreply.github.com>
2024-05-08 16:31:03 +0800

    Add files via upload

---
+++ src/pages/Suppliers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from "../components/Sidebar";
+
+const Suppliers = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Suppliers
